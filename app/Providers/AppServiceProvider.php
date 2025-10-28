23d3b55310dc28b6f2c6fc77da740722202a532d
Ionut180205 <ionut.cioarsa@gmail.com>
2025-10-29 00:07:19 +0200

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Dacă suntem în producție, forțăm schema HTTPS ca să nu apară mixed content.
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
